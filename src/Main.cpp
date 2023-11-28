b8c74d60979051ddb66e4a96d2c4910dfaa75ff1
IsaacAmann <74680621+IsaacAmann@users.noreply.github.com>
2023-11-27 23:22:29 -0600

    Changed to link windows libraries statically. Main lists number of processes currently running

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,11 +3,20 @@
 #include "Main.h"
 #include <detours.h>
 #include <curl.h>
+#include <Windows.h>
+#include <Psapi.h>
 
 int main()
 {
 	std::cout << "test" << '\n';
     DetourAttach(NULL, NULL);
-	
+	DWORD processes[1024], needed, numberProcess;
+
+	EnumProcesses(processes, sizeof(processes), &needed);
+
+	numberProcess = needed / sizeof(DWORD);
+
+	std::cout << "Processes running: " << numberProcess << std::endl;
+
 	return 0;
 }
