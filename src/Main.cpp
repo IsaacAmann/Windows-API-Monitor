e3b6fdae5e08f12e92e300503756751093f29f07
IsaacAmann <74680621+IsaacAmann@users.noreply.github.com>
2023-11-27 23:52:45 -0600

    Prints off process name for each process

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,6 +5,7 @@
 #include <curl.h>
 #include <Windows.h>
 #include <Psapi.h>
+#include <tchar.h>
 
 int main()
 {
@@ -17,6 +18,24 @@ int main()
 	numberProcess = needed / sizeof(DWORD);
 
 	std::cout << "Processes running: " << numberProcess << std::endl;
+
+	for (int i = 0; i < numberProcess; i++)
+	{
+		TCHAR currentName[MAX_PATH] = TEXT("Blank");
+		HMODULE mod;
+		DWORD cbNeeded;
+
+		HANDLE process = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processes[i]);
+
+		if (EnumProcessModules(process, &mod, sizeof(mod), &cbNeeded))
+		{
+			GetModuleBaseName(process, mod, currentName, sizeof(currentName) / sizeof(TCHAR));
+		}
+
+		_tprintf(TEXT("%s  (PID: %u)\n"), currentName, i);
+
+		CloseHandle(process);
+	}
 
 	return 0;
 }
