3aa160168c357e57e08e4f197c0ea070a52f9d6c
IsaacAmann <74680621+IsaacAmann@users.noreply.github.com>
2023-11-27 22:39:09 -0600

    Linked curl and detours

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,13 @@
+#define CURL_STATICLIB
 
 #include "Main.h"
 #include <detours.h>
+#include <curl.h>
 
 int main()
 {
 	std::cout << "test" << '\n';
     DetourAttach(NULL, NULL);
+	
 	return 0;
 }
