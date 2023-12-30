a0f9346e766a0d525000b6475b92f02e26472995
IsaacAmann <74680621+IsaacAmann@users.noreply.github.com>
2023-12-30 16:22:39 -0600

    Trampoline function created correctly for GetCurrentProcessId API call

--- MonitorDLL/dllmain.cpp
+++ MonitorDLL/dllmain.cpp
@@ -1,11 +1,15 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 
-typedef DWORD (WINAPI* defTrampolineFunc)();
+typedef  DWORD (__stdcall *defTrampolineFunc)();
+
+typedef void(voidTrampolineFunc)();
 
 void hookAPICallSimple();
 
 void hookWithTrampoline();
+
+void* createTrampoline(void* functionAddress);
 
 DWORD RedirectGetCurrentProcessId();
 DWORD _stdcall trampolineGetCurrentProcessId();
@@ -95,6 +99,9 @@ void hookWithTrampoline()
     memcpy(patch + 1, &relativeOffset, 4);
     WriteProcessMemory(GetCurrentProcess(), (LPVOID)functionAddress, patch, 5, NULL);
 
+    //Could not get this method of creating the trampoline function to work
+    //Was getting access violations 
+    /*
     trampolineAddress = VirtualAlloc(NULL, 11, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
     hookAddress = (DWORD*)((DWORD)functionAddress + 5);
     memcpy((BYTE*)trampolineAddress, &buffer, 5);
@@ -104,6 +111,25 @@ void hookWithTrampoline()
     //xC3 is RETN assembly instruction
     memcpy((BYTE*)trampolineAddress + 10, "\xC3", 1);
     std::cout << "trampoline address: " << trampolineAddress << std::endl;
+    */
+    trampolineAddress = createTrampoline((void*)functionAddress);
+}
+
+void* createTrampoline(void* functionAddress)
+{
+    uint8_t* trampoline = (uint8_t*) VirtualAlloc(NULL, 10, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    memcpy(trampoline, functionAddress, 5);
+
+    trampoline[5] = 0xE9;
+
+    uint32_t destination = (uint32_t)functionAddress + 5;
+    uint32_t source = (uint32_t)trampoline + 10;
+    uint32_t delta = destination - source;
+    memcpy(trampoline + 6, &delta, 4);
+
+    //VirtualProtect(trampoline, 10, PAGE_EXECUTE_READ, NULL);
+
+    return trampoline;
 }
 
 DWORD _stdcall trampolineGetCurrentProcessId()
@@ -111,8 +137,8 @@ DWORD _stdcall trampolineGetCurrentProcessId()
     std::cout << "GetCurrentProcessId Called" << std::endl;
     std::cout << "trampoline address: " << trampolineAddress << std::endl;
 
-    defTrampolineFunc trampoline = (defTrampolineFunc)trampolineAddress;
-    return trampoline();
+    defTrampolineFunc trampoline = (defTrampolineFunc) trampolineAddress;
+    return 1;
 }
 
 DWORD _stdcall RedirectGetCurrentProcessId()
--- src/Monitoring/TrackedProcess.cpp
+++ src/Monitoring/TrackedProcess.cpp
@@ -9,7 +9,7 @@ TrackedProcess::TrackedProcess(HANDLE processHandle, DWORD PID)
 	processRunning = true;
 	getProcessInfo();
 	//printProcessInfo();
-	if (PID == 7928)
+	if (PID == 20424)
 	{
 		attach();
 		std::cout << libPath << std::endl;
