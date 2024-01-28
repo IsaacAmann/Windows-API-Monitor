1a636686fbf44120f4b24576e11d45b1b75c4350
IsaacAmann <74680621+IsaacAmann@users.noreply.github.com>
2024-01-27 19:47:02 -0600

    Finished hooking advapi32 calls

--- PolyHookMonitorDLL/Hooks/advapi32/advapi32Hooks.cpp
+++ PolyHookMonitorDLL/Hooks/advapi32/advapi32Hooks.cpp
@@ -6,6 +6,39 @@ const bool PRINT_CALLS = true;
 SC_HANDLE(__stdcall* origOpenSCManagerA)(LPCSTR lpMachineName, LPCSTR lpDatabaseName, DWORD dwDesiredAccess) = OpenSCManagerA;
 SC_HANDLE(__stdcall* origOpenSCManagerW)(LPCWSTR lpMachineName, LPCWSTR lpDatabaseName, DWORD dwDesiredAccess) = OpenSCManagerW;
 
+SC_HANDLE(__stdcall* origCreateServiceA)(
+	SC_HANDLE hSCManager,
+	LPCSTR lpServiceName,
+	LPCSTR lpDisplayName,
+	DWORD dwDesiredAccess,
+	DWORD dwServiceType,
+	DWORD dwStartType,
+	DWORD dwErrorControl,
+	LPCSTR lpBinaryPathName,
+	LPCSTR lpLoadOrderGroup,
+	LPDWORD lpdwTagId,
+	LPCSTR lpDependencies,
+	LPCSTR lpServiceStartName,
+	LPCSTR lpPassword) = CreateServiceA;
+SC_HANDLE(__stdcall* origCreateServiceW)(
+	SC_HANDLE hSCManager,
+	LPCWSTR lpServiceName,
+	LPCWSTR lpDisplayName,
+	DWORD dwDesiredAccess,
+	DWORD dwServiceType,
+	DWORD dwStartType,
+	DWORD dwErrorControl,
+	LPCWSTR lpBinaryPathName,
+	LPCWSTR lpLoadOrderGroup,
+	LPDWORD lpdwTagId,
+	LPCWSTR lpDependencies,
+	LPCWSTR lpServiceStartName,
+	LPCWSTR lpPassword) = CreateServiceW;
+
+BOOL(__stdcall* origStartServiceCtrlDispatcherA)(const SERVICE_TABLE_ENTRYA* lpServiceStartTable) = StartServiceCtrlDispatcherA;
+BOOL(__stdcall* origStartServiceCtrlDispatcherW)(const SERVICE_TABLE_ENTRYW* lpServiceStartTable) = StartServiceCtrlDispatcherW;
+
+
 void hookadvapi32APICalls(std::unordered_map<std::string, APICallCounter*>* hooks)
 {
 
@@ -20,6 +53,14 @@ void hookadvapi32APICalls(std::unordered_map<std::string, APICallCounter*>* hook
 	DetourAttach(&origOpenSCManagerA, hookOpenSCManagerA);
 	DetourAttach(&origOpenSCManagerW, hookOpenSCManagerW);
 
+	currentCounter = new APICallCounter("CreateService", hooks);
+	DetourAttach(&origCreateServiceA, hookCreateServiceA);
+	DetourAttach(&origCreateServiceW, hookCreateServiceW);
+
+	currentCounter = new APICallCounter("StartServiceCtrlDispatcher", hooks);
+	DetourAttach(&origStartServiceCtrlDispatcherA, hookStartServiceCtrlDispatcherA);
+	DetourAttach(&origStartServiceCtrlDispatcherW, hookStartServiceCtrlDispatcherW);
+
 	DetourTransactionCommit();
 }
 
@@ -31,6 +72,7 @@ SC_HANDLE __stdcall hookOpenSCManagerA(LPCSTR lpMachineName, LPCSTR lpDatabaseNa
 
 	return origOpenSCManagerA(lpMachineName, lpDatabaseName, dwDesiredAccess);
 }
+
 SC_HANDLE __stdcall hookOpenSCManagerW(LPCWSTR lpMachineName, LPCWSTR lpDatabaseName, DWORD dwDesiredAccess)
 {
 	if (PRINT_CALLS)
@@ -38,4 +80,84 @@ SC_HANDLE __stdcall hookOpenSCManagerW(LPCWSTR lpMachineName, LPCWSTR lpDatabase
 	counterMap.at("OpenSCManager")->incrementCall();
 
 	return origOpenSCManagerW(lpMachineName, lpDatabaseName, dwDesiredAccess);
+}
+
+SC_HANDLE __stdcall hookCreateServiceA(
+	SC_HANDLE hSCManager,
+	LPCSTR lpServiceName,
+	LPCSTR lpDisplayName,
+	DWORD dwDesiredAccess,
+	DWORD dwServiceType,
+	DWORD dwStartType,
+	DWORD dwErrorControl,
+	LPCSTR lpBinaryPathName,
+	LPCSTR lpLoadOrderGroup,
+	LPDWORD lpdwTagId,
+	LPCSTR lpDependencies,
+	LPCSTR lpServiceStartName,
+	LPCSTR lpPassword
+)
+{
+	if (PRINT_CALLS)
+		std::cout << "Called CreateServiceA\n";
+	counterMap.at("CreateService")->incrementCall();
+
+	return origCreateServiceA(
+		hSCManager, lpServiceName,
+		lpDisplayName, dwDesiredAccess, 
+		dwServiceType, dwStartType, 
+		dwErrorControl, lpBinaryPathName,
+		lpLoadOrderGroup, lpdwTagId,
+		lpDependencies, lpServiceName,
+		lpPassword
+	);
+}
+
+SC_HANDLE __stdcall hookCreateServiceW(
+	SC_HANDLE hSCManager,
+	LPCWSTR lpServiceName,
+	LPCWSTR lpDisplayName,
+	DWORD dwDesiredAccess,
+	DWORD dwServiceType,
+	DWORD dwStartType,
+	DWORD dwErrorControl,
+	LPCWSTR lpBinaryPathName,
+	LPCWSTR lpLoadOrderGroup,
+	LPDWORD lpdwTagId,
+	LPCWSTR lpDependencies,
+	LPCWSTR lpServiceStartName,
+	LPCWSTR lpPassword
+)
+{
+	if (PRINT_CALLS)
+		std::cout << "Called CreateServiceW\n";
+	counterMap.at("CreateService")->incrementCall();
+
+	return origCreateServiceW(
+		hSCManager, lpServiceName,
+		lpDisplayName, dwDesiredAccess,
+		dwServiceType, dwStartType,
+		dwErrorControl, lpBinaryPathName,
+		lpLoadOrderGroup, lpdwTagId,
+		lpDependencies, lpServiceName,
+		lpPassword
+	);
+}
+
+BOOL hookStartServiceCtrlDispatcherA(const SERVICE_TABLE_ENTRYA* lpServiceStartTable)
+{
+	if (PRINT_CALLS)
+		std::cout << "Called StartServiceCtrlDispatcherA\n";
+	counterMap.at("StartCtrlDispatcher")->incrementCall();
+
+	return origStartServiceCtrlDispatcherA(lpServiceStartTable);
+}
+
+BOOL hookStartServiceCtrlDispatcherW(const SERVICE_TABLE_ENTRYW* lpServiceStartTable)
+{
+	if (PRINT_CALLS)
+		std::cout << "Called StartServiceCtrlDispatcherW\n";
+	counterMap.at("StartCtrlDispatcher")->incrementCall();
+
+	return origStartServiceCtrlDispatcherW(lpServiceStartTable);
 }
--- PolyHookMonitorDLL/Hooks/advapi32/advapi32Hooks.h
+++ PolyHookMonitorDLL/Hooks/advapi32/advapi32Hooks.h
@@ -9,3 +9,40 @@ void hookadvapi32APICalls(std::unordered_map<std::string, APICallCounter*>* hook
 
 SC_HANDLE __stdcall hookOpenSCManagerA(LPCSTR lpMachineName, LPCSTR lpDatabaseName, DWORD dwDesiredAccess);
 SC_HANDLE __stdcall hookOpenSCManagerW(LPCWSTR lpMachineName, LPCWSTR lpDatabaseName, DWORD dwDesiredAccess);
+
+SC_HANDLE __stdcall hookCreateServiceA(
+	SC_HANDLE hSCManager, 
+	LPCSTR lpServiceName, 
+	LPCSTR lpDisplayName,
+	DWORD dwDesiredAccess,
+	DWORD dwServiceType,
+	DWORD dwStartType,
+	DWORD dwErrorControl,
+	LPCSTR lpBinaryPathName,
+	LPCSTR lpLoadOrderGroup,
+	LPDWORD lpdwTagId,
+	LPCSTR lpDependencies,
+	LPCSTR lpServiceStartName,
+	LPCSTR lpPassword
+);
+
+SC_HANDLE __stdcall hookCreateServiceW(
+	SC_HANDLE hSCManager,
+	LPCWSTR lpServiceName,
+	LPCWSTR lpDisplayName,
+	DWORD dwDesiredAccess,
+	DWORD dwServiceType,
+	DWORD dwStartType,
+	DWORD dwErrorControl,
+	LPCWSTR lpBinaryPathName,
+	LPCWSTR lpLoadOrderGroup,
+	LPDWORD lpdwTagId,
+	LPCWSTR lpDependencies,
+	LPCWSTR lpServiceStartName,
+	LPCWSTR lpPassword
+);
+
+BOOL hookStartServiceCtrlDispatcherA(const SERVICE_TABLE_ENTRYA* lpServiceStartTable);
+BOOL hookStartServiceCtrlDispatcherW(const SERVICE_TABLE_ENTRYW* lpServiceStartTable);
+
+
